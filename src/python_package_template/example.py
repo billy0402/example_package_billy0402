93432d92d1436483c3b6ec101b9b34d6c910365d
Billy Huang <10646003@ntub.edu.tw>
2024-07-31 17:10:56 +0800

    feat(example): add get_google_title

--- src/python_package_template/example.py
+++ src/python_package_template/example.py
@@ -1,2 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def add_one(number):
     return number + 1
+
+
+def get_google_title():
+    response = requests.get("https://www.google.com")
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.find("title").text
+    print(title)
+    return title
